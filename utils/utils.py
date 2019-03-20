6a4ed0300cc85ffd2d93e11c1219c48730f1ca73
Joe Sykes <js1n15@soton.ac.uk>
2019-03-20 12:35:43 +0000

    Add utils file

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 ''' Enum for the types of message that are produced
 '''
 class MessageType():
-  JOIN = 0
+  JOIN_REQUEST = 0
   JOIN_ACK = 1
   SENSOR_RESPONSE = 2
   SENSOR_REQUEST = 3
@@ -27,22 +27,20 @@ class Packet():
 
   @staticmethod
   def createJoinRequestPacket(id):
-    pkt = Packet()
-    pkt.src_id = bytes(id)
-    pkt.dest_id = bytes(0)
-    pkt.msg_type = bytes(MessageType.JOIN_REQUEST)
-    pkt.payload = bytes(0)
+    src_id = bytes(id)
+    dest_id = bytes(0)
+    msg_type = bytes(MessageType.JOIN_REQUEST)
+    payload = bytes(0)
 
-    return pkt
+    return Packet(src_id, dest_id, msg_type, payload)
 
   @staticmethod
   def createJoinResponsePacket(id):
-    pkt = Packet()
-    pkt.src_id = 0
-    pkt.dest_id = id
-    pkt.msg_type = MessageType.JOIN_ACK
-    pkt.payload = 0
-    return pkt
+    src_id = 0
+    dest_id = id
+    msg_type = MessageType.JOIN_ACK
+    payload = 0
+    return Packet(src_id, dest_id, msg_type, payload)
 
   @staticmethod
   def decode_packet(data):
@@ -57,9 +55,4 @@ class Packet():
 
   @staticmethod
   def encode_packet(packet):
-    src = bytes(packet.src_id)
-    dest = bytes(packet.dest_id)
-    type = bytes(packet.msg_type)
-    payload = bytes(packet.payload)
-
-    return bytearray(src, dest, type, payload)
+    return bytearray([packet.src_id, packet.dest_id, packet.type, packet.payload])
